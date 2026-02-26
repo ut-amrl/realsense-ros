081a656c9b6810ed9ff4d1348df5ce467e316c37
rwik2000 <rwik2000@gmail.com>
2026-02-25 21:39:38 -0600

    adding launchfile for alphatruck

---
+++ realsense2_camera/launch/rs_alphatruck_launch.py
@@ -0,0 +1,32 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    realsense_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('realsense2_camera'),
+                'launch',
+                'rs_launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'rgb_camera.color_profile': '424x240x30',
+            'depth_module.depth_profile': '424x240x30',
+            'enable_infra1': 'false',
+            'enable_infra2': 'false',
+            'enable_gyro': 'false',
+            'enable_accel': 'false',
+            'pointcloud.enable': 'true',
+            'decimation_filter.enable': 'true',
+            'decimation_filter.filter_magnitude': '4',
+        }.items()
+    )
+
+    return LaunchDescription([
+        realsense_launch
+    ])
