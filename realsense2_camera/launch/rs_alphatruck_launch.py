160bb86811b450ddbc66e30748f504b76185f05b
rwik2000 <rwik2000@gmail.com>
2026-03-03 11:38:35 -0600

    reverting back to 640 480 30

--- realsense2_camera/launch/rs_alphatruck_launch.py
+++ realsense2_camera/launch/rs_alphatruck_launch.py
@@ -15,8 +15,8 @@ def generate_launch_description():
             ])
         ]),
         launch_arguments={
-            'rgb_camera.color_profile': '424x240x30',
-            'depth_module.depth_profile': '424x240x30',
+            'rgb_camera.color_profile': '640x480x30',
+            'depth_module.depth_profile': '640x480x30',
             'enable_infra1': 'false',
             'enable_infra2': 'false',
             'enable_gyro': 'false',
@@ -24,6 +24,7 @@ def generate_launch_description():
             'pointcloud.enable': 'true',
             'decimation_filter.enable': 'true',
             'decimation_filter.filter_magnitude': '4',
+            'align_depth.enable': 'true',
         }.items()
     )
 
